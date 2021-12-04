7ff8609e177b7b3d25f221f99c6fe099597d19e3
unique-chan <yechankim@gm.gist.ac.kr>
2021-12-04 21:41:26 +0900

    ResidualBlock

---
+++ Model/ResidualBlock.py
@@ -0,0 +1,23 @@
+from torch import nn
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size,
+                               stride=stride, padding=padding, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size,
+                               stride=stride, padding=padding, bias=False)
+        self.bn = nn.BatchNorm2d(num_features=out_channels)
+        self.p_relu = nn.PReLU()
+        self.short_cut = nn.Sequential()
+
+    def forward(self, x):
+        identity = x
+        out = self.conv1(x)
+        out = self.bn(out)
+        out = self.p_relu(out)
+        out = self.conv2(out)
+        out = self.bn(out)
+        out = self.short_cut(identity) + out
+        return out
