fcf110bd185dedce68a99658aff23f352f384c05
unique-chan <yechankim@gm.gist.ac.kr>
2021-12-05 01:41:17 +0900

    minor typo change

--- Model/ConvBlock.py
+++ Model/ConvBlock.py
@@ -5,7 +5,7 @@ class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
         super(ConvBlock, self).__init__()
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding=1),
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(0.2)
         )
