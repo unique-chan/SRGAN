8adb6f4b5cb22e35b2ba8f820284fec8d8acb99b
unique-chan <yechankim@gm.gist.ac.kr>
2021-12-04 22:15:35 +0900

    UpsamlingBlock

---
+++ Model/UpsamlingBlock.py
@@ -0,0 +1,17 @@
+from torch import nn
+
+
+class UpsamlingBlock(nn.Module):
+    def __init__(self, in_channels, scaling_factor, kernel_size=3, stride=1, padding=1):
+        super(UpsamlingBlock, self).__init__()
+        out_channels = (in_channels * scaling_factor) ** 2
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size,
+                               stride=stride, padding=padding, bias=False)
+        self.pixel_shuffle = nn.PixelShuffle(scaling_factor)
+        self.p_relu = nn.PReLU()
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.pixel_shuffle(out)
+        out = self.p_relu(out)
+        return out
