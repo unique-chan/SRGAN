c961288434559e5fcfb54a718549c3a412d42e4e
unique-chan <yechankim@gm.gist.ac.kr>
2021-12-12 01:24:54 +0900

    device +

--- ContentLoss/ResNetLoss.py
+++ ContentLoss/ResNetLoss.py
@@ -5,14 +5,14 @@ from ContentLoss.ContentLoss_utils import get_extractor
 
 
 class ResNetLoss(nn.Module):
-    def __init__(self, i=5, j=4, resnet_model_name='resnet34'):
+    def __init__(self, i=5, j=4, device='cuda:0', resnet_model_name='resnet34'):
         ''' [Logic]
         1) extract the feature map from the 'j'-th convolutional layer before 'i'-th max-pooling layer
            for both hr_image (ground_truth) and sr_image (prediction).
         2) compute and return mse_loss between features maps of hr_image & sr_image.
         '''
         super(ResNetLoss, self).__init__()
-        resnet = torch.hub.load('pytorch/vision:v0.10.0', resnet_model_name, pretrained=True).eval()
+        resnet = torch.hub.load('pytorch/vision:v0.10.0', resnet_model_name, pretrained=True).to(device).eval()
         self.extractor = get_extractor(resnet, i, j)
         self.mse_loss = nn.MSELoss()
 
--- ContentLoss/VGGLoss.py
+++ ContentLoss/VGGLoss.py
@@ -5,14 +5,14 @@ from ContentLoss.ContentLoss_utils import get_extractor
 
 
 class VGGLoss(nn.Module):
-    def __init__(self, i=5, j=4, vgg_model_name='vgg19_bn'):
+    def __init__(self, i=5, j=4, device='cuda:0', vgg_model_name='vgg19_bn'):
         ''' [Logic]
         1) extract the feature map from the 'j'-th convolutional layer before 'i'-th max-pooling layer
            for both hr_image (ground_truth) and sr_image (prediction).
         2) compute and return mse_loss between features maps of hr_image & sr_image.
         '''
         super(VGGLoss, self).__init__()
-        vgg = torch.hub.load('pytorch/vision:v0.10.0', vgg_model_name, pretrained=True).eval()
+        vgg = torch.hub.load('pytorch/vision:v0.10.0', vgg_model_name, pretrained=True).to(device).eval()
         self.extractor = get_extractor(vgg, i, j)
         self.mse_loss = nn.MSELoss()
 
